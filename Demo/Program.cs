8ffa4a30a5dcf513b4ca628c42f31ae82b9796b6
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-18 16:22:21 +0300

    V-2

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -8,15 +8,41 @@ namespace Demo
             #region V-1
             // What You Can Write Inside?
         // 1. Signature For Method
-        void Print();
+        //void Print();
 
-        // 2. Signature For Property
-        public int X { get; set; }
+        //// 2. Signature For Property
+        //public int X { get; set; }
 
-        // 3. Default Implemented Method
-        private void PrintHello()
+        //// 3. Default Implemented Method
+        //private void PrintHello()
+        //{
+        //    Console.WriteLine("Say Hello");
+        //}
+        #endregion
+        #region V-2
+        // Access Modifier Allowed Inside ?
+
+        //private
+        private int X;
+        //Private Protected
+        //private protected int Z;
+        //Protected
+        protected int Y;
+        //Internal
+        internal int W;
+        //protected Internal
+        protected internal int B;
+        //Public
+        public int H;
+
+        public void SetX(int value)
         {
-            Console.WriteLine("Say Hello");
+            X = value;
+            Y = value;
+            Z = value;
+            W = value;
+            H = value;
+            B = value;
         }
         #endregion
     }
