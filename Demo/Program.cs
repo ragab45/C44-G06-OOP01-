f7d7716effde71e573325b19a46760dd369343a0
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-18 16:20:56 +0300

    V-1

---
+++ Demo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+
+namespace Demo
+{
+    internal class Program
+    {
+        
+            #region V-1
+            // What You Can Write Inside?
+        // 1. Signature For Method
+        void Print();
+
+        // 2. Signature For Property
+        public int X { get; set; }
+
+        // 3. Default Implemented Method
+        private void PrintHello()
+        {
+            Console.WriteLine("Say Hello");
+        }
+        #endregion
+    }
+    
+}
