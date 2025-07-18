78d57e67e4aa2b29dc368f81df1ba1b004bdc178
ragab45 <115900223+ragab45@users.noreply.github.com>
2025-07-18 16:40:33 +0300

    Q-1

--- Assemente/Program.cs
+++ Assemente/Program.cs
@@ -2,9 +2,18 @@
 {
     internal class Program
     {
+        
+            #region Q-1
+             enum WeekDays { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Days of the week:");
+            foreach (WeekDays day in Enum.GetValues(typeof(WeekDays)))
+            {
+                Console.WriteLine(day);
+            }
         }
+        #endregion
     }
-}
+    }
